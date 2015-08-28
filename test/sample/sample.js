816ae878b918913dd9a6602e9c9cbdf62d1910ce
Yuri Neves Silveira <yurinevessilveira@gmail.com>
2015-08-28 00:05:09 -0300

    glob

---
+++ test/sample/sample.js
@@ -0,0 +1,28 @@
+// - -------------------------------------------------------------------- - //
+
+"use strict";
+
+var fs = require("fs");
+var assert = require("assert");
+var Crawler = require("bauer-crawler");
+
+var crawler = new Crawler({
+  config: {
+    glob: {
+      
+    }
+  }
+});
+
+crawler.loadPlugin(__dirname + "/../../");
+
+crawler.start(function() {
+  
+  return this.promise()
+    .glob(__dirname + "/*.js")
+    .then(function(files) {
+      assert.deepEqual(files,[__dirname + "/sample.js"]);
+    });
+});
+
+// - -------------------------------------------------------------------- - //
